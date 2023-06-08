d3ae983e09ac6a2ae5f074c29550d2cb0e23ddc6
romain.piveteau <r.piveteau@outlook.com>
2023-06-08 17:15:53 +0200

    Exo 4 1

--- src/Arena.js
+++ src/Arena.js
@@ -17,7 +17,50 @@ class Arena {
   // Gestion du déplacement du héros
   move(direction) {
     /* Your code goes here */ 
-  
+    const oldPosition = {x: this.hero.x, y: this.hero.y}
+    if ( !this.canMove(oldPosition, direction)) return oldPosition;
+    switch(direction) {
+      case "N":
+        this.hero.y -= 1;
+        break;
+      case "S":
+        this.hero.y += 1;
+        break;
+      case "W":
+        this.hero.x -= 1;
+        break;
+      case "E":
+        this.hero.x += 1;
+        break;
+      default:
+        break;
+    }
+
     return oldPosition;
+  }
+
+  canMove(oldPosition, direction) {
+    if (direction === "W" && oldPosition.x) return this.isPositionAvailable({
+      ...oldPosition,
+      x: oldPosition.x - 1
+    })
+    if (direction === "E" && oldPosition.x < this.size - 1) return this.isPositionAvailable({
+      ...oldPosition,
+      x: oldPosition.x + 1
+    })
+    if (direction === "N" && oldPosition.y) return this.isPositionAvailable({
+      ...oldPosition,
+      y: oldPosition.y - 1
+    })
+    if (direction === "S" && oldPosition.y < this.size - 1) return this.isPositionAvailable({
+      ...oldPosition,
+      y: oldPosition.y + 1
+    })
+    
+    return false;
+  }
+
+  isPositionAvailable({x, y}) {// New position
+    return !this.monsters.find((m) => (m.x === x && m.y === y))
   }
 }
